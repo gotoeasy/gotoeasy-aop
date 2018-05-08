79d55650ebb201407f24e9439ce72addf945fabc
zhangming <sunmoon2222@163.com>
2018-05-08 09:33:25 +0800

    tst

---
+++ src/test/java/top/gotoeasy/sample/aop/sample1/Sample1MainTest.java
@@ -0,0 +1,12 @@
+package top.gotoeasy.sample.aop.sample1;
+
+import org.junit.Test;
+
+public class Sample1MainTest {
+
+    @Test
+    public void test() {
+        Sample1Main.main(null);
+    }
+
+}
